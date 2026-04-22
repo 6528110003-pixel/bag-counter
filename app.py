b056bea8608dd826a67ebc76aa2cf3a2db707702
6528110003-pixel <6528110003@neu.ac.th>
2026-04-22 11:15:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,27 @@
 from flask import Flask, render_template, request
 from ultralytics import YOLO
-import cv2
 import os
 
 app = Flask(__name__)
 
-model = YOLO("best.pt")
-
 UPLOAD_FOLDER = "uploads"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
+
 @app.route("/", methods=["GET", "POST"])
 def index():
+
     count = None
     image_path = None
 
     if request.method == "POST":
+
         file = request.files["image"]
         path = os.path.join(UPLOAD_FOLDER, file.filename)
         file.save(path)
+
+        # โหลดโมเดลตอนใช้งาน
+        model = YOLO("best.pt")
 
         results = model(path)
 
@@ -28,8 +31,8 @@ def index():
 
     return render_template("index.html", count=count, image=image_path)
 
-import os
 
 if __name__ == "__main__":
+
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
