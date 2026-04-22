0a9106d7590f3266ddfa18340da5d4feebf27f8f
6528110003-pixel <6528110003@neu.ac.th>
2026-04-22 10:49:29 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+from ultralytics import YOLO
+import cv2
+import os
+
+app = Flask(__name__)
+
+model = YOLO("best.pt")
+
+UPLOAD_FOLDER = "uploads"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    count = None
+    image_path = None
+
+    if request.method == "POST":
+        file = request.files["image"]
+        path = os.path.join(UPLOAD_FOLDER, file.filename)
+        file.save(path)
+
+        results = model(path)
+
+        count = len(results[0].boxes)
+
+        image_path = path
+
+    return render_template("index.html", count=count, image=image_path)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
