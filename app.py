7d2f5c84e7d5535ebf9f8c7ccdfcc8c71c29d4f7
6528110003-pixel <6528110003@neu.ac.th>
2026-04-22 11:05:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -28,5 +28,8 @@ def index():
 
     return render_template("index.html", count=count, image=image_path)
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
